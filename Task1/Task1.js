ed6ae1954c91c48af1a33d3bf2e885671f4acfa3
runner855 <runner855@icloud.com>
2024-03-03 22:59:20 +0100

    task 1 complete

--- Task1/Task1.js
+++ Task1/Task1.js
@@ -3,3 +3,12 @@
 // che possa  anche  contenere  stringhe  doppie,  
 // restituisca  un  array  ordinato  in  ordine  alfabetico 
 // senza  stringhe  doppie.
+
+const Names = ["Laura", "James", "Laura", "John", "John", "Mike", "Anthony", "Bob"];
+
+
+const removeDuplicates = (data) => {
+  return data.filter((name, index) => data.indexOf(name) === index);
+}
+
+console.log(removeDuplicates(Names.sort()))
